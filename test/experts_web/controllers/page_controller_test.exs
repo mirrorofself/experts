5ee296b5b84a18e8032a81c1e3adb698fbff2eec
Tomaz Zlender <tomaz.zlender@gmail.com>
2019-10-02 05:54:26 +0200

    Remove default root page content

--- test/experts_web/controllers/page_controller_test.exs
+++ test/experts_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule ExpertsWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Expert Advice"
   end
 end
